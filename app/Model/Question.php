0245ee810ce2a872fbdeceb4bc2ea65d69b794b8
Adebola <adebola@Adebolas-MacBook-Pro.local>
2019-10-19 00:54:53 +0100

    database and factory coniguration

---
+++ app/Model/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Model;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
