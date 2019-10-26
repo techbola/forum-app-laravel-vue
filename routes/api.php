235d6ed3c7fd5d4c8ff9ab8b83166fc2a43e3ee6
Adebola <adebola@Adebolas-MacBook-Pro.local>
2019-10-26 16:22:30 +0100

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,17 @@ Route::apiResource('/question/{question}/reply', 'ReplyController');
 
 Route::post('/reply/{reply}/like', 'LikeController@likeIt');
 Route::delete('/reply/{reply}/like', 'LikeController@unLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
